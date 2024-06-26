3e47ca78db3c03ba48857f0502b3524794fc59ab
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-06-26 04:16:59 -0300

    Estrutura inicial projeto Spring-boot

---
+++ src/test/java/com/vx/microservices/java/event_microservice/EventMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vx.microservices.java.event_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
